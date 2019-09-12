7ccf5f334b60483da7d4294a824b06166e012ee7
Gareth <gareth.kh.lau@gmail.com>
2019-09-12 10:04:00 -0400

    coolio

---
+++ 09122019/index.js
@@ -0,0 +1,6 @@
+console.log("1" + 1);   // 11 - treats this as string concatentation
+console.log("5" - 1);   // 4
+console.log(Infinity - Infinity); // NAN - not a number
+
+let x = null;
+console.log(x ? "x is not null" : "x is null");
